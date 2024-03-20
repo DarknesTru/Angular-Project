8b34aa2c44ba3f8025c672525fdb5503d9f68921
sebas <sebastianop@msmk.university>
2024-03-20 13:38:07 +0100

    booleanos y operadores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,10 @@ export class AppComponent {
   data1: number = 0;
   data2: number = 0;
 
+  ngOnInit() {
+    this.executets();
+  }
+  
   public setData1(event :any) {
     this.data1 = Number(event.target.value)
   }
@@ -48,7 +52,61 @@ export class AppComponent {
     console.log()
   }
 
+executets() {
+  const nombre: string = "Sebas";
 
+const numero: number = 1;
+
+const arrayNumero: number[] = [5,8,99,-7,];
+
+const tryeFalse: boolean = true;
+
+const tryeFalseArray: boolean[] = [true, false, true, false];
+
+let datoVacio: number;
+
+datoVacio = 5;
+datoVacio = 25; 
+
+const numero1: number = 1;
+const numero2: number = 2;
+
+
+const operadores: string[] = ["+", "-", "*", "/", "&&", "||", "===", "!=="];
+const comparacion: boolean = (1===1); 
+const comparacion2: boolean = (numero1===2); 
+
+console.log("tiene que devolver:true", comparacion);
+console.log("tiene que devolver:false", comparacion2);
+
+console.log("tiene que devolver:true", numero1!==numero2)
+console.log("tiene que devolver:false", 2!==numero2)
+
+console.log("operador &&:true$$true", true&&true);
+console.log("operador &&:true$$false", true&&false);
+console.log("operador &&:false$$true", false&&true);
+console.log("operador &&:false$$false", false&&false);
+
+console.log("operador ||:true$$true", true||true);
+console.log("operador ||:true$$false", true||false);
+console.log("operador ||:false$$true", false||true);
+console.log("operador ||:false$$false", false||false);
+
+console.log("pregunta1:devuelve true", (numero1!==2)||false);
+console.log("pregunta2:devuelve false", (numero1!==1)||false);
+console.log("pregunta3:devuelve true", (numero1!==1||true);
+console.log("pregunta4:devuelve false", (numero1!==2)&&false);
+
+let condition: boolean = true; 
+
+if(condition){
+  console.log("entramos en el if");
+
+} else {
+  console.log("no entramos en el if")
+}
+
+}
 
 
 
